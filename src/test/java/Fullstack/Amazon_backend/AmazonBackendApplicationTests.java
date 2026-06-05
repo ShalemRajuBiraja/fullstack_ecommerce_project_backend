ef9bd517e013964a45b6b3d2b2ae2f833b3570a9
Shalem Raju <shalemrajubiraja7@gmail.com>
2026-06-05 15:12:53 +0530

    amazon backend project setup

---
+++ src/test/java/Fullstack/Amazon_backend/AmazonBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Fullstack.Amazon_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
